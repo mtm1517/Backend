bfdaa6ddf3bb390e60e763f6d70bb282dfade4b5
VicsonMoses <32441021+VicsonMoses@users.noreply.github.com>
2020-05-04 09:58:25 -0700

    Add files via upload

---
+++ backend_api/login.js
@@ -0,0 +1,42 @@
+import  React , { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "./login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const dbRoute = 'mongodb://<myvu>:<Mtm151782>';
+
+
+
+
+  function Submit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={Submit}>
+        <FormGroup controlId="email" bsStyle="primary" bsSize="large">
+          <FormLabel>Email</FormLabel>
+          <FormControl
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsStyle="primary" bsSize="large">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="small" type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
